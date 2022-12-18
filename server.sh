fb7562f70af06c570281ccc78212bb73a558c303
stmunguy <Minh.P.b.Nguyen@stud.h-da.de>
2022-12-18 20:45:01 +0100

    push aktueller Stand

--- server.sh
+++ server.sh
@@ -1,24 +1,34 @@
 
 receive(){
-     if  [ -s registerClient.txt ]; then
+     if  [ -s registerClient.txt  ]; then
         echo "" | cat registerClient.txt
         succeedMessage
-        #args=$(cat registerClient.txt)
-        #echo "works with $args"
+        args=$(cat registerClient.txt)
+        echo "works with $args"
 
         truncate -s 0 registerClient.txt
      fi
 }
+receiveCredentials(){
+
+  if [ -z "$args" ] ; then
+     return
+  else
+       echo "works with $args"
+       succeedMessage
+  fi
+}
 
 
 succeedMessage(){
-  readData=$(cat registerClient.txt)
-  echo "Server sends back: $readData" | timeout 1 nc -n 127.0.0.1 55555
-
+  #readData=$(cat registerClient.txt)
+  echo "Server sends back: $args" | timeout 1 nc -n 127.0.0.1 55555
+  args=noArgs
 }
 
 connectionToServer(){
-  nc -l -p 54321 -v > registerClient.txt &
+
+  args=$(nc -l -p 54321 -v &)
   echo "Server is running"
 }
 
@@ -31,9 +41,9 @@ lamportAuthorisation(){
       fi
   done
 }
-#succeedMessage
+
 connectionToServer
 while true
 do
-    receive
+    receiveCredentials
 done
